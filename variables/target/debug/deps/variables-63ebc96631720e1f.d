693adf5ec5cee6f409ff1271831052277ff37128
Kyle Wright <kylejamwright@gmail.com>
2025-01-13 22:03:07 -0700

    gitignore

--- variables/target/debug/deps/variables-63ebc96631720e1f.d
+++
@@ -1,5 +0,0 @@
-/Users/kylewright/development/rust/practice/variables/target/debug/deps/variables-63ebc96631720e1f: src/main.rs
-
-/Users/kylewright/development/rust/practice/variables/target/debug/deps/variables-63ebc96631720e1f.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/kylewright/development/rust/practice/variables/target/debug/variables: /Users/kylewright/development/rust/practice/variables/src/main.rs
